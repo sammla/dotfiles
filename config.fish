3b1b7f098599a1e651bd88325e39485d1cee514d
Martin Paulucci <martin.c.paulucci@gmail.com>
2013-06-19 14:47:26 -0300

    changes

--- config.fish
+++ config.fish
@@ -1,49 +1,19 @@
-set PATH $PATH /sbin
-set PATH $PATH /usr/sbin
-
 set fish_plugins git node
-
 
 alias gs="git status"
 
-function fish_prompt --description 'Write out the prompt'
-    # Just calculate these once, to save a few cycles when displaying the prompt
-    if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-    end
- 
-    if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
-    end
- 
-    if not set -q __git_cb
-    set __git_cb "("(set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)")"
-    end
- 
-    switch $USER
- 
-    case root
- 
-    if not set -q __fish_prompt_cwd
-        if set -q fish_color_cwd_root
-            set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-        else
-            set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-        end
-    end
- 
-    printf '%s@%s:%s%s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
- 
-    case '*'
- 
-    if not set -q __fish_prompt_cwd
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-    end
- 
-    printf '%s@%s:%s%s%s%s$ ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
- 
-    end
-end
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
 
+# Theme
+set fish_theme numist 
 
-cd ~/workspace/qa-tool
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish cofiguration.
+. $fish_path/oh-my-fish.fish
